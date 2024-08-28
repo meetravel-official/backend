7de92944b3aef7c912bbc28ec708c63362f9c961
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-28 14:13:09 +0900

    feat: 로그인/인증

---
+++ src/main/java/com/meetravel/domain/auth/dto/response/LoginResponse.java
@@ -0,0 +1,27 @@
+package com.meetravel.domain.auth.dto.response;
+
+import com.meetravel.domain.user.enums.SocialType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+
+    private String userId;
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+    private LocalDateTime accessTokenExpiresAt;
+    private LocalDateTime refreshTokenExpiresAt;
+    private SocialType socialType;
+    private boolean registeredUserYn;
+
+}
+
