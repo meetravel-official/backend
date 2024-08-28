cbd16be51205061047900bc89f7741fe4ca165a6
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-28 14:16:36 +0900

    feat: time utils

---
+++ src/main/java/com/meetravel/global/utils/TimeUtils.java
@@ -0,0 +1,29 @@
+package com.meetravel.global.utils;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class TimeUtils {
+
+    private TimeUtils() {
+
+    }
+
+    /**
+     * Date -> LocalDateTime 변환
+     *
+     * @param date
+     * @return
+     */
+    public static LocalDateTime convertDateToLocalDateTime(Date date) {
+        if (date == null) {
+            throw new IllegalArgumentException("Date cannot be null");
+        }
+        return Instant.ofEpochMilli(date.getTime())
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+    }
+
+}
