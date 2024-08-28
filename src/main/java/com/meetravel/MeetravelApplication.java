114e7a5a5c32cf0acfe8bd79bd6b9dbd6ff60d4c
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-28 14:11:48 +0900

    feat: application

---
+++ src/main/java/com/meetravel/MeetravelApplication.java
@@ -0,0 +1,13 @@
+package com.meetravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meetravel/MeetravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
