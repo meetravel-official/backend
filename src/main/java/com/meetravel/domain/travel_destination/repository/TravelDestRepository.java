abcbcf20e334e573644007d134b2eb47638533a5
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-31 20:35:16 +0900

    fix: 변수명 변경에 따른 수정

--- src/main/java/com/meetravel/domain/travel_destination/repository/TravelDestRepository.java
+++ src/main/java/com/meetravel/domain/travel_destination/repository/TravelDestRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface TravelDestRepository extends JpaRepository<TravelDestEntity, TravelDest> {
-    Optional<TravelDestEntity> findByTravelDestId(Long travelDestId);
+public interface TravelDestRepository extends JpaRepository<TravelDestEntity, Long> {
+    Optional<TravelDestEntity> findById(Long travelDestId);
 }
