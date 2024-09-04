992536574a19e7d288453e0f6ed249d502ae2082
JaesungYoun <jayjoy05@khu.ac.kr>
2024-09-04 21:32:16 +0900

    fix: xml로 반환되는 버그 수정

--- src/main/java/com/meetravel/domain/tour_api/TourApiAreaFeignClient.java
+++ src/main/java/com/meetravel/domain/tour_api/TourApiAreaFeignClient.java
@@ -4,26 +4,27 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "korServiceClient", url = "http://apis.data.go.kr/B551011/KorService")
-public interface TourApiAreaFeginClient {
+@FeignClient(name = "tourApiAreaFeignClient", url = "http://apis.data.go.kr/B551011/KorService1")
+public interface TourApiAreaFeignClient {
 
-    @GetMapping(value = "/areaCode", consumes = "application/x-www-form-urlencoded;charset=utf-8")
+    @GetMapping(value = "/areaCode1",produces = "application/json")
     TourApiAreaResponse getArea(
-            @RequestParam("serviceKey") String serviceKey,
-            @RequestParam("numOfRows") int numOfRows,
-            @RequestParam("pageNo") int pageNo,
-            @RequestParam("MobileOS") String mobileOS,
-            @RequestParam("MobileApp") String mobileApp,
-            @RequestParam("_type") String type
-    );
-
-    TourApiDetailAreaResponse getDetailArea(
-            @RequestParam("serviceKey") String serviceKey,
             @RequestParam("numOfRows") int numOfRows,
             @RequestParam("pageNo") int pageNo,
             @RequestParam("MobileOS") String mobileOS,
             @RequestParam("MobileApp") String mobileApp,
             @RequestParam("_type") String type,
-            @RequestParam(value = "areaCode") String areaCode
+            @RequestParam("serviceKey") String serviceKey
+    );
+
+    @GetMapping(value = "/areaCode1",produces = "application/json")
+    TourApiDetailAreaResponse getDetailArea(
+            @RequestParam("numOfRows") int numOfRows,
+            @RequestParam("pageNo") int pageNo,
+            @RequestParam("MobileOS") String mobileOS,
+            @RequestParam("MobileApp") String mobileApp,
+            @RequestParam("areaCode") String areaCode,
+            @RequestParam("_type") String type,
+            @RequestParam("serviceKey") String serviceKey
     );
 }
