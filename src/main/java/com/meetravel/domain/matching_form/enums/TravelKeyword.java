22b89ab56af5f998baae6f3d1eda5dad79098b97
JaesungYoun <jayjoy05@khu.ac.kr>
2024-09-12 20:04:12 +0900

    feat: 여행 키워드

---
+++ src/main/java/com/meetravel/domain/matching_form/enums/TravelKeyword.java
@@ -0,0 +1,39 @@
+package com.meetravel.domain.matching_form.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.meetravel.global.enums.BaseEnum;
+import lombok.Getter;
+
+@Getter
+public enum TravelKeyword implements BaseEnum {
+    MOUNTAIN("산"),
+    SEA("바다"),
+    CITY("도시"),
+    SUBURB("근교"),
+    CAMPING("캠핑"),
+    HIKING("걷기여행"),
+    NIGHT_VIEW("야경"),
+    TRADITIONAL_CULTURE("전통문화"),
+    LIFESTYLE_TOURISM("생활관광"),
+    LEISURE_EXPERIENCE("레저체험"),
+    RELAXATION("휴양"),
+    HEALING("힐링");
+
+    @JsonValue
+    private final String value;
+
+    TravelKeyword(String value) {
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static GroupSize fromValueToEnum(String value) {
+        for (GroupSize size : GroupSize.values()) {
+            if (size.getValue().equals(value)) {
+                return size;
+            }
+        }
+        return null;
+    }
+}
