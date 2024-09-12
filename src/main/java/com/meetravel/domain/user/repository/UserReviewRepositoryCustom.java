1341033cdd7ca098d370b5b937a49988f203ea2c
JaesungYoun <jayjoy05@khu.ac.kr>
2024-09-12 20:35:19 +0900

    remove: 유저 리뷰 기능 삭제

--- src/main/java/com/meetravel/domain/user/repository/UserReviewRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.meetravel.domain.user.repository;
-
-import com.meetravel.domain.user.enums.Review;
-
-import java.util.List;
-
-public interface UserReviewRepositoryCustom {
-    List<Review> getUserReview(String userId);
-}
