5c4110be519c63fcd98f2db694bf18d48231aecc
JaesungYoun <jayjoy05@khu.ac.kr>
2024-09-12 20:04:40 +0900

    feat: 여행 키워드 엔티티

---
+++ src/main/java/com/meetravel/domain/matching_form/entity/TravelKeywordEntity.java
@@ -0,0 +1,35 @@
+package com.meetravel.domain.matching_form.entity;
+
+import com.meetravel.domain.matching_form.enums.TravelKeyword;
+import com.meetravel.global.audit.BaseEntity;
+import com.meetravel.global.converter.TravelKeywordConverter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "travel_keyword")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TravelKeywordEntity extends BaseEntity {
+
+    @Id
+    @Column(name = "TRAVEL_KEYWORD_ID")
+    private Long id;
+
+    @Column(name = "TRAVEL_KEYWORD")
+    @Convert(converter = TravelKeywordConverter.class)
+    private TravelKeyword keyword;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MATCHING_FORM_ID", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private MatchingFormEntity matchingForm;
+
+    public void setMatchingForm(MatchingFormEntity matchingForm){
+        this.matchingForm = matchingForm;
+    }
+}
