1ee5116b217c4f6285c5efa647acabb7eb7ba6bf
JaesungYoun <jayjoy05@khu.ac.kr>
2024-09-22 19:12:18 +0900

    feat: 마이페이지 정보 수정, 닉네임 수정, 회원 탈퇴

--- src/main/java/com/meetravel/domain/user/controller/UserController.java
+++ src/main/java/com/meetravel/domain/user/controller/UserController.java
@@ -1,17 +1,17 @@
 package com.meetravel.domain.user.controller;
 
 
+import com.meetravel.domain.user.dto.request.UpdateMyPageInfoRequest;
+import com.meetravel.domain.user.dto.request.UpdateNicknameRequest;
 import com.meetravel.domain.user.dto.response.GetMyPageResponse;
 import com.meetravel.domain.user.service.UserService;
 import com.meetravel.global.jwt.JwtService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -25,7 +25,22 @@ public class UserController implements UserControllerDoc {
     @Override
     @GetMapping("/{userId}/my-page")
     public ResponseEntity<GetMyPageResponse> getMyPage(String userId) {
+        log.info("Get MyPage");
         return ResponseEntity.ok(userService.getMyPage(userId));
+    }
+
+
+    @Override
+    @PutMapping("/{userId}/nickname")
+    public void updateNickname(@RequestParam String userId, @RequestBody @Valid UpdateNicknameRequest request) {
+        log.info("Update Nickname");
+        userService.updateNickname(userId, request.getNickname());
+    }
+    @Override
+    @PutMapping("/{userId}/info")
+    public void updateMyPageInfo(@RequestParam String userId, @RequestBody @Valid UpdateMyPageInfoRequest request){
+        log.info("Update MyPageInfo");
+        userService.updateMyPageInfo(userId, request);
     }
 
     /**
@@ -39,6 +54,13 @@ public class UserController implements UserControllerDoc {
         log.info("logout");
         String token = jwtService.getToken(request);
         userService.logout(token);
+    }
+
+    @Override
+    @DeleteMapping("/delete")
+    public void deleteUser(@RequestParam String userId) {
+        log.info("Delete User");
+        userService.deleteUser(userId);
     }
 
 
--- src/main/java/com/meetravel/domain/user/controller/UserControllerDoc.java
+++ src/main/java/com/meetravel/domain/user/controller/UserControllerDoc.java
@@ -1,11 +1,16 @@
 package com.meetravel.domain.user.controller;
 
+import com.meetravel.domain.user.dto.request.UpdateMyPageInfoRequest;
+import com.meetravel.domain.user.dto.request.UpdateNicknameRequest;
 import com.meetravel.domain.user.dto.response.GetMyPageResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "사용자(유저) API")
 public interface UserControllerDoc {
@@ -13,6 +18,15 @@ public interface UserControllerDoc {
     @Operation(summary = "마이페이지 조회", description = "마이페이지를 조회합니다.")
     ResponseEntity<GetMyPageResponse> getMyPage(@PathVariable String userId);
 
+    @Operation(summary = "닉네임 수정", description = "닉네임을 수정합니다.")
+    void updateNickname(String userId, UpdateNicknameRequest request);
+
+    @Operation(summary = "마이페이지 부가 정보를 수정", description = "마이페이지 부가 정보를 수정합니다.")
+    void updateMyPageInfo(String userId, UpdateMyPageInfoRequest request);
+
     @Operation(summary = "회원 로그아웃", description = "회원 로그아웃을 진행합니다.")
     void logout(HttpServletRequest request);
+
+    @Operation(summary = "회원 탈퇴", description = "회원 탈퇴를 진행합니다.")
+    void deleteUser(String userId);
 }
