cf1226695066a50119cf668103424f34636e805d
JaesungYoun <jayjoy05@khu.ac.kr>
2024-09-23 02:13:23 +0900

    fix: 회원가입 완료 시, 정식토큰 발급

--- src/main/java/com/meetravel/domain/sign_up/controller/SignUpController.java
+++ src/main/java/com/meetravel/domain/sign_up/controller/SignUpController.java
@@ -1,8 +1,11 @@
 package com.meetravel.domain.sign_up.controller;
 
+import com.meetravel.domain.auth.dto.response.LoginResponse;
 import com.meetravel.domain.sign_up.dto.request.SignUpRequest;
 import com.meetravel.domain.sign_up.dto.response.GetSignUpInfoList;
 import com.meetravel.domain.sign_up.service.SignUpService;
+import com.meetravel.global.jwt.JwtService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,12 +19,20 @@ import org.springframework.web.bind.annotation.*;
 public class SignUpController implements SignUpControllerDoc {
 
     private final SignUpService signUpService;
+    private final JwtService jwtService;
 
     @Override
     @PostMapping("")
-    public void signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
+    public void signUp(@RequestBody @Valid SignUpRequest signUpRequest, HttpServletResponse response) {
         log.info("회원가입");
         signUpService.signUp(signUpRequest);
+
+
+        // 회원가입 완료 토큰 발급
+        LoginResponse loginResponse = jwtService.createJwtToken(signUpRequest.getUserId(), false);
+        // 헤더에 토큰 싣기
+        jwtService.setHeaderAccessToken(response, loginResponse.getAccessToken());
+        jwtService.setHeaderRefreshToken(response, loginResponse.getRefreshToken());
     }
 
 
--- src/main/java/com/meetravel/domain/sign_up/controller/SignUpControllerDoc.java
+++ src/main/java/com/meetravel/domain/sign_up/controller/SignUpControllerDoc.java
@@ -4,6 +4,7 @@ import com.meetravel.domain.sign_up.dto.request.SignUpRequest;
 import com.meetravel.domain.sign_up.dto.response.GetSignUpInfoList;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Tag(name = "회원가입 관련 API")
 public interface SignUpControllerDoc {
     @Operation(summary = "회원가입", description = "회원가입을 진행합니다.")
-    void signUp(@RequestBody @Valid SignUpRequest signUpRequest);
+    void signUp(@RequestBody @Valid SignUpRequest signUpRequest, HttpServletResponse response);
 
     @Operation(summary = "닉네임 중복 확인", description = "닉네임 중복 확인을 진행합니다.")
     ResponseEntity<Boolean> checkNicknameDuplicate(@RequestParam String nickName);
