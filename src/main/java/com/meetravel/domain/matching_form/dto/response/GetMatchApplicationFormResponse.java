99f12c732bcb2cc93fa25c9bf7962b8ccbb6dff4
chae0929c <won@storelink.io>
2024-09-23 16:18:48 +0900

    docs: 매칭 로직 스웨거 추가

--- src/main/java/com/meetravel/domain/matching_form/dto/response/GetMatchApplicationFormResponse.java
+++ src/main/java/com/meetravel/domain/matching_form/dto/response/GetMatchApplicationFormResponse.java
@@ -1,9 +1,14 @@
 package com.meetravel.domain.matching_form.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class GetMatchApplicationFormResponse {
+    @Schema(
+            description = "매칭 신청서 고유 번호",
+            nullable = true
+    )
     private final Long matchingFormId;
 
     public GetMatchApplicationFormResponse() {
